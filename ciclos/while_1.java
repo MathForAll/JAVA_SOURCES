77d79ae810ec0ad9788d962f70aa1d76ea807eff
MathForAll <128271640+MathForAll@users.noreply.github.com>
2023-03-18 23:10:25 -0600

    Create while_1.java

---
+++ ciclos/while_1.java
@@ -0,0 +1,26 @@
+package ciclos;
+
+public class ciclo_while_1 
+{
+
+	public static void main(String[] args) 
+	{
+		int i = 10000; 
+		
+		while(i <= 500)
+		{
+			System.out.println(i);
+			i += 5;
+		}
+		/*
+		do
+		{
+			System.out.println(i);
+			i += 5;
+		}
+		while(i <= 500);*/
+		
+		System.exit(0);
+	}
+
+}
